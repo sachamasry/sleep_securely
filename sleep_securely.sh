e643518938dce1cf0487824286398d9ce4416613
sacha <sacha@sachamasry.com>
2025-03-08 14:13:40 +0000

    Debugged not perfectly working aspects of the script, such as requesing that closed apps quit, such as Parallels Desktop, resulting in the application starting in order to quit. Added further qualifications such as around the Tailscale command. Added a whole slew of sensitive communications applications which should not be open for outside attention at system wake.
    FossilOrigin-Name: 05edac634ff6cf99a9667ccd46e1dc448b252d9e8486b3b88289ce2dd37c2390

--- sleep_securely.sh
+++ sleep_securely.sh
@@ -3,8 +3,10 @@
 # Quit KeePassXC upon sleep
 /usr/bin/killall KeePassXC
 
-# Tell Parallels Desktop application to quit gently
-osascript -e 'tell application "Parallels Desktop" to quit'
+# Tell Parallels Desktop application to quit gently, if it is already open
+if pgrep -q "Application Name"; then
+    osascript -e 'tell application "Parallels Desktop" to quit'
+fi
 
 # Disable Wi-Fi
 networksetup -setairportpower en0 off
@@ -18,7 +20,32 @@ diskutil list external | grep -E '^\/' | while read -r volume; do
 done
 
 # Disable Tailscale
-/Applications/Tailscale.app/Contents/MacOS/Tailscale down
+if [ -x "/Applications/Tailscale.app/Contents/MacOS/Tailscale" ]; then
+    "/Applications/Tailscale.app/Contents/MacOS/Tailscale" down
+fi
 
 # Mute system volume to prevent unpleasant surprises!
 osascript -e "set volume with output muted"
+
+# Close all email applications (MUAs)
+pkill -x "Proton Mail"
+pkill -x "Thunderbird"
+pkill -x "thunderbird"
+pkill -x "Mail"
+
+# Close sensitive communication applications
+pkill -x "Discord"
+pkill -x "FaceTime"
+pkill -x "Messages"
+pkill -x "Microsoft Teams"
+pkill -x "Signal"
+pkill -x "Slack"
+pkill -x "Telegram"
+pkill -x "Trello"
+pkill -x "Viber"
+pkill -x "WhatsApp"
+
+pkill -x "Zoom"
+pkill -x "Zoom.us"
+pkill -x "zoom"
+pkill -x "zoom.us"
